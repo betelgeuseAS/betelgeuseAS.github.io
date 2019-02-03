832edd9cc779b7e777d45783c22c74dcd8ae1256
Andrew <betelgeuse.as@gmail.com>
2019-02-03 11:31:23 +0200

    Notes

---
+++ Notes/precache-manifest.7474ed3a52e2002f4e2ae312a831e639.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c59f42dadf1a1ea7a31",
+    "url": "/static/css/main.1e7a823e.chunk.css"
+  },
+  {
+    "revision": "8c59f42dadf1a1ea7a31",
+    "url": "/static/js/main.8c59f42d.chunk.js"
+  },
+  {
+    "revision": "8f5cf498f758da446e71",
+    "url": "/static/js/1.8f5cf498.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2808c53df23746067c2456994eb57a26",
+    "url": "/index.html"
+  }
+];
